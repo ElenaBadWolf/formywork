da6c581807bbf3550992747ccf21213fedf557d9
ElenaBadWolf <plastmassovaya@gmail.com>
2016-04-26 18:56:24 +0300

    Delete test_add_event.py

--- test_add_event.py
+++
@@ -1,8 +0,0 @@
-
-
-def test_add_event_admin(app):
-    app.add_event_admin.login_admin()
-    app.add_event_admin.chose_event()
-    app.add_event_admin.click_add_event()
-    app.add_event_admin.fill_form_event()
-    app.add_event_admin.have_alook_onsite()
